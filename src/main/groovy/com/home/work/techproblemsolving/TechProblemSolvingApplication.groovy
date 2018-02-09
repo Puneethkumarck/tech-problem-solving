e7294731a63cfc7bfe9b8403beff062d1cf00909
PuneethKumarCk <punith.564@gmail.com>
2018-02-09 23:40:09 +0530

    initial setup

---
+++ src/main/groovy/com/home/work/techproblemsolving/TechProblemSolvingApplication.groovy
@@ -0,0 +1,12 @@
+package com.home.work.techproblemsolving
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TechProblemSolvingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run TechProblemSolvingApplication, args
+	}
+}
